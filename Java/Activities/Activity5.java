773170d067258a868dde4cf3267d7dd6506da3cc
Aryan Sharma <73022435+ARYANSHARMA18@users.noreply.github.com>
2026-06-12 11:24:26 +0530

    Added Day-3 Activities

---
+++ Java/Activities/Activity5.java
@@ -0,0 +1,10 @@
+package activities;
+
+public class Activity5 {
+	public static void main(String[] str) {
+		MyBook myNovel = new MyBook();
+		
+		myNovel.setTitle("Story of my life!");
+		System.out.println("The title is: "+ myNovel.getTitle());
+	}
+}
